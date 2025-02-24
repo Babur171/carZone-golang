fa0bed2e0e6618189f7d011f81a4b4324d3a5cfc
babur <baburali894@gmail.com>
2025-02-24 19:17:31 +0500

    Add new modals

--- models/engine.go
+++ models/engine.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Engine struct {
+	EngineID       uuid.UUID `json:"engine_id"`
+	Displacement   int64     `json:"displacement"`
+	NoOfCyclinders int64     `json:"noOfCyclinders"`
+	CarRange       int64     `json:"carRange"`
+}
